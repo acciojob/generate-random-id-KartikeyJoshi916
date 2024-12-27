f1e400e7748fe00f62bf836d6e56031058b644bc
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-12-28 03:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function makeid(l) {
 	let s="QWERTYUIOPASDFGHJKLMNBVCXZqwertyuiopasdfghjklzxcvbnm1234567890";
 	let result="";
 	for(let i=0;i<l;i++){
-		int x=Math.random()*l;
+		int x=Math.random()*s.length;
 		result+=s.charAt(x);
 	}
 	return result;
